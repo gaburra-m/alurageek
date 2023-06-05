444e46d54abe89d87418192ff98cb9af40c51b21
Gabriel Morales <gaburra.m@gmail.com>
2023-06-05 14:00:41 -0600

    cambios admin-service

--- service/admin-service.js
+++ service/admin-service.js
@@ -2,15 +2,15 @@ const URL = "http://localhost:3000/productos"
 
 const listaProductos = () => fetch(URL).then(respuesta => respuesta.json())
 
-const crearProducto = (nombre, img, precio, categoria, desc) =>{
+const crearProducto = (nombre, img, precio, categoria, desc) => {
     return fetch(URL, {
         method: "POST",
         headers: {
-        "Content-Type": "application/json"
+            "Content-Type": "application/json"
         },
-        body: JSON.stringify({nombre, img, precio, categoria, desc, id: uuid.v4()})
+        body: JSON.stringify({ nombre, img, precio, categoria, desc, id: uuid.v4() })
     })
-    }
+}
 
 const eliminarProducto = (id) => {
     return fetch(`${URL}/${id}`, {
@@ -18,26 +18,26 @@ const eliminarProducto = (id) => {
     })
 }
 
-const detalleProducto = (id) => {
-    return fetch(`${URL}/${id}`).then(respuesta => respuesta.json())
+const detalleProducto = async (id) => {
+    return await fetch(`${URL}/${id}`).then(respuesta => respuesta.json())
 }
 
-const actualizarProducto = (nombre, img, precio, categoria, desc, id) => {
-    return fetch(`${URL}/${id}`, {
+const actualizarProducto = async (nombre, img, precio, categoria, desc, id) => {
+    return await fetch(`${URL}/${id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({nombre, img, precio, categoria, desc})
+        body: JSON.stringify({ nombre, img, precio, categoria, desc })
     })
-    .then((respuesta) => respuesta)
-    .catch(error => console.log(error))
+        .then((respuesta) => respuesta)
+        .catch(error => console.log(error))
 }
 
-    export const adminServices = {
+export const adminServices = {
     listaProductos,
     crearProducto,
     eliminarProducto,
     detalleProducto,
     actualizarProducto
-    }
+}
