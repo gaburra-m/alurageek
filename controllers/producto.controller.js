711d59edf5bcbc189ddd48da0be4109cd7183989
Gabriel Morales <gaburra.m@gmail.com>
2023-06-13 18:59:18 -0600

    Consolas page

--- controllers/producto.controller.js
+++ controllers/producto.controller.js
@@ -1,6 +1,6 @@
 import { adminServices } from "../service/admin-service.js";
 
-const nuevaListaProducto = (nombre, img, precio,categoria, id) => {
+const nuevaListaProducto = (nombre, img, precio, categoria, id) => {
     const article = document.createElement("article")
     const contenido = `
         <img class="card__img" src="../assets/img/productos/${img}" alt="${nombre}">
@@ -25,7 +25,7 @@ const nuevaListaProducto = (nombre, img, precio,categoria, id) => {
     eliminar.addEventListener("click", () => {
         const id = eliminar.id
         adminServices.eliminarProducto(id)
-        .then( () => {}).catch(error => console.log(error))
+            .then(() => { }).catch(error => console.log(error))
     })
     return article
 }
@@ -33,8 +33,41 @@ const nuevaListaProducto = (nombre, img, precio,categoria, id) => {
 const listProduct = document.querySelector("[data-admin-products]")
 
 adminServices.listaProductos().then((data) => {
-    data.forEach( ({ nombre, img, precio,categoria, id }) => {
+    data.forEach(({ nombre, img, precio, categoria, id }) => {
         const nuevoProducto = nuevaListaProducto(nombre, img, precio, categoria, id)
         listProduct.appendChild(nuevoProducto)
     })
 }).catch((error) => console.log(error))
+
+//Home page
+
+adminServices.listaProductos().then((data) => {
+    const resp = data.filter(dato => dato.categoria === 'consolas')
+    resp.forEach(({ nombre, img, precio, categoria, id }) => {
+        const nuevoProducto = nuevaHomeProducto(nombre, img, precio, categoria, id)
+        homeProduct.appendChild(nuevoProducto)
+    })
+}).catch((error) => console.log(error))
+
+const homeProduct = document.querySelector("[data-home-products]")
+
+const nuevaHomeProducto = (nombre, img, precio, categoria) => {
+    const article = document.createElement("article")
+    const contenido = `
+        <img class="card__img" src="../assets/img/productos/${img}" alt="${nombre}">
+        <div class="card__wrapper">
+            <h4 class="card__title">${nombre}</h4>
+            <p class="card__price">$${precio}</p>
+            
+            <div class="card__link--btn">
+                    <a class="card__link" href="./producto.html"
+                      >Ver Producto</a
+                    >
+                  </div>
+        </div>
+    `
+    article.classList.add("card")
+    article.innerHTML = contenido
+
+    return article
+}
